e925cf5d83328ea49997de68b635b9af0a1987c5
Kirill Omelchenko <kirill@adsys.com>
2015-09-04 11:20:02 +0300

    OutputReader now can stop;Memory leack fix

--- models/output_reader.py
+++ models/output_reader.py
@@ -1,7 +1,6 @@
 __author__ = 'twisted'
 from PyQt4 import QtCore
 
-
 class OutputReader(QtCore.QThread):
     def __init__(self, cmd_runner=None, _callback_object=None):
         QtCore.QThread.__init__(self)
@@ -9,18 +8,27 @@ class OutputReader(QtCore.QThread):
         self.__callback_object = None
         self.set_command_runner(cmd_runner)
         self.set_output_callback_object(_callback_object)
+        self.__stop = False
 
     def set_command_runner(self, cmd_runner):
         self.__cmdr = cmd_runner
 
+    # obsolete
     def set_output_callback_object(self, _callback_object):
         self.__callback_object = _callback_object
 
     def run(self):
         if self.__cmdr is not None:
-            while True:
-                line = self.__cmdr.get_process().stdout.readline()
+            self.__stop = False
+            while not self.__stop:
+                line = \
+                    self.__cmdr.get_process()\
+                        .stdout.readline().decode().replace('\n', '')
+
                 if str(line) != '':
                     self.emit(QtCore.SIGNAL('update(QString)'),
-                              str(line))
+                              line)
+            self.__cmdr.stop()
 
+    def stop(self):
+        self.__stop = True
