d9a2c04714f2108ed3a2091c587e442feb26dd1b
drtwisted <kirill.omelchenko@gmail.com>
2015-09-06 11:06:09 +0300

    add IP validation, file write helpers

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,1 +1,20 @@
 __author__ = 'twisted'
+
+
+def check_ip(ip):
+    def is_valid_octet(octet):
+        return 0 <= int(octet) <= 254
+
+    ip = ip.split('.')
+    if len(ip) != 4:
+        return False
+    elif is_valid_octet(ip[0]) and is_valid_octet(ip[1]) and \
+            is_valid_octet(ip[2]) and is_valid_octet(ip[3]):
+        return True
+    else:
+        return False
+
+
+def write_to_file(fname, data):
+    with open(fname, 'aw') as file:
+        file.write(data)
