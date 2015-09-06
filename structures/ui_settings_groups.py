0a648a60c8a7fb4ff455a534bae8e489bbbecebe
drtwisted <kirill.omelchenko@gmail.com>
2015-09-06 19:01:10 +0300

    Introduce settings.py:set_widgets_visible_state

---
+++ structures/ui_settings_groups.py
@@ -0,0 +1,14 @@
+__author__ = 'twisted'
+
+
+class OptionGroups:
+    app_path = 'AppPath'
+    app_package = 'AppPackage'
+    app_activity = 'AppActivity'
+    wait_for_activity = 'WaitForActivity'
+    device_name = 'DeviceName'
+    device_ready_timeout = 'DeviceReadyTimeout'
+    use_local_time_zone = 'UseLocalTimeZone'
+    server_address = 'ServerAddress'
+    server_port = 'ServerPort'
+    server_command_timeout = 'ServerCommandTimeout'
