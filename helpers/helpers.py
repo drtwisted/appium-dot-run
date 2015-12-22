5e73a1235247ad7bbbcd63d5fbec1387a44d07d5
Kirill Omelchenko <kirill@ad-sys.com>
2015-12-22 11:17:15 +0200

    octests_valid function implementation optimization

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -5,14 +5,25 @@ def check_ip(ip):
     def is_valid_octet(octet):
         return 0 <= int(octet) <= 254
 
+    def octets_valid(ip):
+        is_valid = True
+        if isinstance(list, ip):
+            for i in range(4):
+                is_valid = is_valid and is_valid_octet(ip[i])
+        else:
+            is_valid = False
+
+        return is_valid
+
+    def ip_length_valid(ip):
+        if isinstance(list, ip):
+            return len(ip) == 4
+
+        return False
+
     ip = ip.split('.')
-    if len(ip) != 4:
-        return False
-    elif is_valid_octet(ip[0]) and is_valid_octet(ip[1]) and \
-            is_valid_octet(ip[2]) and is_valid_octet(ip[3]):
-        return True
-    else:
-        return False
+
+    return ip_length_valid(ip) and octets_valid(ip)
 
 
 def write_to_file(fname, data):
